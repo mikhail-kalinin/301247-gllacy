dba0253a1ade97bb6f0349435fad1f1ec0fa61a7
mikhail-kalinin <allgoodyeah@gmail.com>
2017-02-19 15:58:04 +0800

    Прогрессивное улучшение и базовая оптимизация
    Исправил ошибки, добавил анимацию, минимизировал файлы стилей и
    скриптов.

--- js/script.js
+++ js/script.js
@@ -1,28 +1,31 @@
 /*Модальное окно*/
 var link = document.querySelector("#feedback");
 
-var popup = document.querySelector(".modal-feedback");
-var close = popup.querySelector(".btn-close");
+var modal = document.querySelector(".modal-content");
+var overlay = document.querySelector(".modal-overlay");
+var close = modal.querySelector(".btn-close");
 
-var username = popup.querySelector("[name=feedback-name]");
+var username = modal.querySelector("[name=feedback-name]");
 
 link.addEventListener("click", function(event) {
   event.preventDefault();
-  popup.classList.add("modal-feedback-show");
-  username.focus();
+  modal.classList.add("modal-content-show");
+  overlay.classList.add("modal-overlay-show");
 });
 
 close.addEventListener("click", function(event) {
   event.preventDefault();
-  popup.classList.remove("modal-feedback-show");
+  modal.classList.remove("modal-content-show");
+  overlay.classList.remove("modal-overlay-show");
 });
 
 
 
 window.addEventListener("keydown", function(event) {
   if (event.keyCode === 27) {
-    if (popup.classList.contains("modal-feedback-show")) {
-      popup.classList.remove("modal-feedback-show");
+    if (modal.classList.contains("modal-content-show")) {
+      modal.classList.remove("modal-content-show");
+      overlay.classList.remove("modal-overlay-show");
     }
   }
 });
